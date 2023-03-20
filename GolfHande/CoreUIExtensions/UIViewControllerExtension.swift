7f657b5b20afe68731b587240b68af4fdd2be04f
Andy Nam <andynam1997@gmail.com>
2023-03-20 13:00:07 -0700

    Adds comment.

--- GolfHande/CoreUIExtensions/UIViewControllerExtension.swift
+++ GolfHande/CoreUIExtensions/UIViewControllerExtension.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 extension UIViewController {
+
+    /// Hides keyboard when user taps outside of keyboard.
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
                 tap.cancelsTouchesInView = false
