f0e6f951387c7907983f5f9bced1a3a5ad7b1903
Andy Nam <andynam1997@gmail.com>
2023-03-20 15:12:12 -0700

    Implemented calculating handicap and returning single value for different number of rounds.

--- GolfHande/HomeScreenView/MyScoresViewModel.swift
+++ GolfHande/HomeScreenView/MyScoresViewModel.swift
@@ -29,6 +29,7 @@ class MyScoresViewModel {
         }
         
         dispatchGroup.notify(queue: .main) {
+            self.getUsersHandicap()
             completion()
         }
     }
@@ -46,5 +47,95 @@ class MyScoresViewModel {
     func getCourseInfo(for courseID: String) -> GolfCourseData? {
         guard let golfCourse = golfCourseArray.first(where: { $0.id == courseID }) else { return nil }
         return golfCourse
+    }
+
+    func getUsersHandicap() -> String {
+        let usersHandicapString = userScoreArray.map { $0.handicap }
+        let usersHandicap = usersHandicapString.compactMap(Double.init)
+
+        switch usersHandicap.count {
+        case let count where count <= 3:
+            print("Less than 3 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 1, forArray: usersHandicap)
+            let handicap = lowestNumbers[0] - 2.0
+            print(handicap)
+            return ""
+        case let count where count <= 4:
+            print("Less than 4 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 1, forArray: usersHandicap)
+            let handicap = lowestNumbers[0] - 1.0
+            print(handicap)
+            return ""
+        case let count where count <= 5:
+            print("less than 5 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 1, forArray: usersHandicap)
+            let handicap = lowestNumbers[0]
+            print(handicap)
+            return ""
+        case let count where count <= 6:
+            print("Less than 6 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 2, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers) - 1.0
+            print(handicap)
+            return ""
+        case let count where count <= 8:
+            print("Less than 8 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 2, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count <= 11:
+            print("Less than 11 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 3, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count <= 14:
+            print("Less than 14 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 4, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count <= 16:
+            print("Less than 16 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 5, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count <= 18:
+            print("Less than 18 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 6, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count <= 19:
+            print("Less than 19 rounds")
+            let lowestNumbers = getLowestNumbers(forHowMany: 7, forArray: usersHandicap)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        case let count where count >= 20:
+            print("Greater than 20 rounds")
+            let lastTwentyRounds = Array(usersHandicap.prefix(20))
+            let lowestNumbers = getLowestNumbers(forHowMany: 8, forArray: lastTwentyRounds)
+            let handicap = getDoubleAverage(for: lowestNumbers)
+            print(handicap)
+            return ""
+        default:
+            print("hm...")
+            return ""
+        }
+    }
+
+    private func getLowestNumbers(forHowMany: Int,
+                                  forArray: [Double]) -> [Double] {
+        let lowestNumbers = Array(forArray.sorted().prefix(forHowMany))
+        return lowestNumbers
+    }
+
+    private func getDoubleAverage(for userScores: [Double]) -> Double {
+        let sum = userScores.reduce(0, +)
+        let average = sum / Double(userScores.count)
+        return average
     }
 }
